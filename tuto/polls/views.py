4fdde85e081117d07c85de6a6288ad863ba2938a
sahinof <ofrkns@gmail.com>
2021-12-09 21:22:36 +0300

    added new views (detail, results and vote) and urlconfigs associated with them

--- tuto/polls/views.py
+++ tuto/polls/views.py
@@ -5,3 +5,12 @@ from django.shortcuts import render
 
 def index(request):
     return HttpResponse("Hello, world. This is polls index.")
+
+def detail(request, question_id):
+    return HttpResponse("You're looking at question %s." % question_id)
+
+def results(request, question_id):
+    return HttpResponse("You're looking at question %s." % question_id)
+
+def vote(request, question_id):
+    return HttpResponse("You're voting on question %s." % question_id)
